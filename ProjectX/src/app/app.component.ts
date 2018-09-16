b8898dbba974d65b64ae630b09dd21f6b3c6be9e
BurekLord <mile2389i@gmail.com>
2018-09-16 21:02:17 +0200

    Firebase integration

--- ProjectX/src/app/app.component.ts
+++ ProjectX/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'app';
-}
+export class AppComponent {}
