4250fee27f1464b01759773da2806690dd63c56e
Marko Misojcic <misojcicmarko@gmail.com>
2018-09-23 03:00:46 +0200

    some of components

--- ProjectX/src/app/app.module.ts
+++ ProjectX/src/app/app.module.ts
@@ -7,9 +7,14 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { DescriptionComponent } from './main/description/description.component';
+import { LoginComponent } from './main/login/login.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent, DescriptionComponent, LoginComponent],
     imports: [
         BrowserModule,
         FormsModule,
