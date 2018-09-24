65b63935e70241fea10b3bd91028a4344ff7def3
BurekLord <mile2389i@gmail.com>
2018-09-24 21:54:22 +0200

    We finnaly have BE communication services

--- ProjectX/src/services/transaction.service.ts
+++ ProjectX/src/services/transaction.service.ts
@@ -1,12 +1,11 @@
 import { Expense } from './../models/expense.model';
 import { User } from './../models/user.model';
-import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 import { Income } from '../models/income.model';
 
 @Injectable()
 export class TransactionService {
-    constructor(private db: AngularFireDatabase) {}
+    constructor() {}
 
     getAllUserIncomes(user: User): Income[] {
         return undefined;
--- ProjectX/src/services/user.services.ts
+++ ProjectX/src/services/user.services.ts
@@ -1,33 +1,64 @@
-import { User } from './../models/user.model';
-import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import {
+    AngularFirestoreDocument,
+    AngularFirestore,
+    AngularFirestoreCollection
+} from 'angularfire2/firestore';
+import { config } from './config';
 
+import { pipe, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from './../models/user.model';
 @Injectable()
 export class UserService {
-    constructor(private db: AngularFireDatabase) {}
+    usersCollection: AngularFirestoreCollection<User> = this.db.collection<
+        User
+    >(config.users_endpoint);
+    userDocument: AngularFirestoreDocument<User>;
+    usersObservable: Observable<any>;
 
-    getAllUsers(): User[] {
-        // this.af
-        //     .list('/users')
-        //     .push({ name: 'Mile', pass: '123' })
-        //     .then(console.log, console.error);
-        // this.af.list('/users').valueChanges();
-        return undefined;
+    constructor(public db: AngularFirestore) {}
+
+    getAllUsers() {
+        return this.db
+            .collection(config.users_endpoint)
+            .snapshotChanges()
+            .pipe(
+                map(actions => {
+                    return actions.map(a => {
+                        // Get document data
+                        const data = a.payload.doc.data() as User;
+                        // Get document id
+                        const id = a.payload.doc.id;
+                        // Use spread operator to add the id to the document data
+                        return { id, ...data };
+                    });
+                })
+            );
     }
 
-    getUserById(userId: string): User {
-        return undefined;
+    updateUser(id: string, update: User) {
+        this.userDocument = this.db.doc<User>(`${config.users_endpoint}/${id}`);
+        this.userDocument
+            .update(update)
+            .then(console.log)
+            .catch(console.error);
     }
 
     addUser(user: User) {
-        return undefined;
+        return this.usersCollection.add(JSON.parse(JSON.stringify(user)));
     }
 
-    removeUser(user: User) {
-        return undefined;
+    removeUser(id: string) {
+        this.userDocument = this.db.doc<User>(`${config.users_endpoint}/${id}`);
+        this.userDocument
+            .delete()
+            .then(console.log)
+            .catch(console.error);
     }
 
-    updateUser(user: User) {
+    getUser(user: User): User {
         return undefined;
     }
 }
