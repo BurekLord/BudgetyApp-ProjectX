c853617cab2186b0b30236a0f8ace4f5ae188dd4
BurekLord <mile2389i@gmail.com>
2018-09-26 12:34:02 +0200

    setup iinitial

--- ProjectX/src/app/main/setup/setup.component.ts
+++ ProjectX/src/app/main/setup/setup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-setup',
-  templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.scss']
+    selector: 'app-setup',
+    templateUrl: './setup.component.html',
+    styleUrls: ['./setup.component.scss']
 })
 export class SetupComponent implements OnInit {
+    inputText = 'First lets enter yout current balance';
+    btnNextText = 'Next';
+    btnAddText = 'Add';
+    tables = [
+        { name: 'balance', value: ['12312454,123'] },
+        { name: 'common incomes', value: ['cat1', 'cat2'] }
+    ];
+    btnEndText = 'Skip';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
