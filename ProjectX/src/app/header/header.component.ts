3a6faa8bf21ffc68b44ec5a1796d14c40fe2eb13
BurekLord <mile2389i@gmail.com>
2018-09-27 12:51:41 +0200

    fixes

--- ProjectX/src/app/header/header.component.ts
+++ ProjectX/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase/app';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    onSignOUt() {
+        auth()
+            .signOut()
+            .then(function() {
+                console.log('User sign-OUT method called!');
+            })
+            .catch(function(error) {
+                console.log('Error happened while singing OUT!', error);
+            });
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
