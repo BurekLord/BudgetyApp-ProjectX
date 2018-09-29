cb6676de29bd27e6a25e18e6e24c64accf09f03c
BurekLord <mile2389i@gmail.com>
2018-09-29 13:43:51 +0200

    init functionality

--- ProjectX/src/app/main/categories/categories.component.ts
+++ ProjectX/src/app/main/categories/categories.component.ts
@@ -1,12 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../../../models/user.model';
+import { DBService } from './../../../services/db.service';
+import {
+    Component,
+    OnInit,
+    Input,
+    OnChanges,
+    SimpleChanges
+} from '@angular/core';
+import { config } from '../../../services/config';
 
 @Component({
     selector: 'app-categories',
     templateUrl: './categories.component.html',
     styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent implements OnInit {
-    constructor() {}
+export class CategoriesComponent implements OnInit, OnChanges {
+    @Input()
+    userData: User;
+    showInput = false;
+    showBtnFinish = false;
+    constructor(private db: DBService) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        this.userData = changes.userData.currentValue;
+    }
 
     ngOnInit() {}
+
+    onAdd(value: string) {
+        this.showInput = true;
+        this.showBtnFinish = true;
+        console.log(this.userData);
+        if (this.userData) {
+            if (this.userData.getCategoriesExp()) {
+                this.userData.getCategoriesExp().push(value);
+            } else {
+                this.userData.setCategoriesExp([]);
+            }
+        }
+    }
+
+    onFinish() {
+        this.showInput = false;
+        this.showBtnFinish = false;
+        this.db.updateItem<User>(
+            config.users_endpoint,
+            this.userData.getId(),
+            this.userData
+        );
+    }
 }
