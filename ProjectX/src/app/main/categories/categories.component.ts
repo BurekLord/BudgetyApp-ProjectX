a3f265cd7bb13b5171b34f0061a888001920c4dc
Marko Misojcic <misojcicmarko@gmail.com>
2018-10-06 23:56:46 +0200

    added income categories; added total amount of money for each category; formated value of money

--- ProjectX/src/app/main/categories/categories.component.ts
+++ ProjectX/src/app/main/categories/categories.component.ts
@@ -1,5 +1,7 @@
 import { config } from './../../../services/config';
 import { User } from './../../../models/user.model';
+import { Income } from './../../../models/income.model';
+import { Expense } from './../../../models/expense.model';
 import { DBService } from './../../../services/db.service';
 import {
     Component,
@@ -10,6 +12,7 @@ import {
     ViewChild,
     ElementRef
 } from '@angular/core';
+import { expand } from 'rxjs/operators';
 
 @Component({
     selector: 'app-categories',
@@ -18,69 +21,190 @@ import {
 })
 export class CategoriesComponent implements OnInit, OnChanges {
     //
-    @ViewChild('input')
-    input: ElementRef;
+    @ViewChild('expInput')
+    expInput: ElementRef;
+    @ViewChild('incInput')
+    incInput: ElementRef;
+
+    user: User;
+    expenses: Expense[];
+    incomes: Income[];
+    currency: String;
 
     @Input()
-    userData: User;
-    showInput = false;
-    showBtnFinish = false;
+    set userData(value) {
+        this.user = value;
+    }
+    @Input()
+    set userExpenses(value) {
+        this.expenses = value;
+    }
+    @Input()
+    set userIncomes(value) {
+        this.incomes = value;
+    }
+    // values to display from this array for expsense categories
+    expCategoryValuePairs = [];
+    // values to display from this array for income categories
+    incCategoryValuePairs = [];
+
+    expInputShow = false;
+    expBtnFinish = false;
+
+    incInputShow = false;
+    incBtnFinish = false;
+
     constructor(private db: DBService) {}
 
     clearInputFields() {
-        this.input.nativeElement.value = null;
+        this.expInput.nativeElement.value = null;
+        this.incInput.nativeElement.value = null;
     }
 
-    ngOnChanges(changes: SimpleChanges) {
-        this.userData = changes.userData.currentValue;
-        if (this.userData) {
-            if (!this.userData.getCategoriesExp()) {
-                this.userData.setCategoriesExp([]);
+    // format money values to dot separated string e.g. 4.000
+    formatMoney(money: number): string {
+        // currency will be worked on, string literal for now
+        this.currency = '$';
+        // array of digits form given number
+        let digits: string[];
+        digits = money.toString().split('');
+        // final array of formated number with dots to be converted to string
+        const formatedDigits: string[] = [];
+        let formatedMoney = '';
+        let dotPoint = digits.length - 1;
+        for (let i = digits.length - 1; i > -1; i--) {
+            if (i === dotPoint - 3) {
+                formatedDigits.unshift(digits[i] + '.');
+                dotPoint -= 3;
+            } else {
+                formatedDigits.unshift(digits[i]);
             }
         }
+        // create final string joining elements of formatedDigits array
+        formatedMoney = formatedDigits.join('') + this.currency;
+        return formatedMoney;
+    }
+
+    // get total money spent by Category
+    totalExpByCat() {
+        this.expCategoryValuePairs = [];
+        // temporary array with all categories
+        const tmpCatArr: string[] = this.user.getCategoriesExp();
+        // loop through all categories
+        tmpCatArr.forEach(cat => {
+            let catVal = 0;
+            // loop through all expenses
+            this.expenses.forEach(exp => {
+                // compare category values from tmpCatArr and expense array
+                if (cat === exp.getCategory()) {
+                    // if true add all expense values(converted to positives) for current category
+                    catVal += Math.abs(exp.getValue());
+                }
+            });
+            this.expCategoryValuePairs.push([cat, this.formatMoney(catVal)]);
+        });
+    }
+
+    // get total money gained by Category
+    totalIncByCat() {
+        this.incCategoryValuePairs = [];
+        // temporary array with all categories
+        const tmpCatArr: string[] = this.user.getCategoriesInc();
+        // loop through all categories
+        tmpCatArr.forEach(cat => {
+            let catVal = 0;
+            // loop through all expenses
+            this.incomes.forEach(inc => {
+                // compare category values from tmpCatArr and expense array
+                if (cat === inc.getCategory()) {
+                    // if true add all expense values(converted to positives) for current category
+                    catVal += inc.getValue();
+                }
+            });
+            this.incCategoryValuePairs.push([cat, this.formatMoney(catVal)]);
+        });
+    }
+    ngOnChanges(changes: SimpleChanges) {
+        // if (this.userData) {
+        //     if (!this.userData.getCategoriesExp()) {
+        //         this.userData.setCategoriesExp([]);
+        //     }
+        // }
+        this.totalExpByCat();
+        this.totalIncByCat();
     }
 
     ngOnInit() {}
 
-    onAdd(value: string) {
-        this.showInput = true;
-        this.showBtnFinish = true;
-        console.log(this.userData);
+    onAdd(value: string, type: string) {
+        if (type === 'expense') {
+            this.expInputShow = true;
+            this.expBtnFinish = true;
+        } else if (type === 'income') {
+            this.incInputShow = true;
+            this.incBtnFinish = true;
+        }
+        console.log(this.user);
         if (value) {
-            if (this.userData) {
-                if (this.userData.getCategoriesExp()) {
-                    this.userData.getCategoriesExp().push(value);
-                    this.db.updateItem<User>(
-                        config.users_endpoint,
-                        this.userData.getId(),
-                        this.userData
-                    );
+            if (this.user) {
+                if (type === 'expense') {
+                    this.user
+                        .getCategoriesExp()
+                        .push(
+                            value[0].toUpperCase() +
+                                value.slice(1).toLowerCase()
+                        );
+                } else if (type === 'income') {
+                    this.user
+                        .getCategoriesInc()
+                        .push(
+                            value[0].toUpperCase() +
+                                value.slice(1).toLowerCase()
+                        );
                 }
+                this.db.updateItem<User>(
+                    config.users_endpoint,
+                    this.user.getId(),
+                    this.user
+                );
             }
         }
         this.clearInputFields();
     }
 
-    onFinish() {
-        this.showInput = false;
-        this.showBtnFinish = false;
+    onFinish(type: string) {
+        if (type === 'expense') {
+            this.expInputShow = false;
+            this.expBtnFinish = false;
+        }
+        if (type === 'income') {
+            this.incInputShow = false;
+            this.incBtnFinish = false;
+        }
+
         this.db.updateItem<User>(
             config.users_endpoint,
-            this.userData.getId(),
-            this.userData
+            this.user.getId(),
+            this.user
         );
         this.clearInputFields();
     }
 
-    onRemove(cat: any) {
+    onRemove(cat: any, type: string) {
         // ovo moze mnoooooogo bolje, al sam lenj
-        this.userData
-            .getCategoriesExp()
-            .splice(this.userData.getCategoriesExp().indexOf(cat), 1);
+        if (type === 'expense') {
+            this.user
+                .getCategoriesExp()
+                .splice(this.user.getCategoriesExp().indexOf(cat), 1);
+        } else if (type === 'income') {
+            this.user
+                .getCategoriesInc()
+                .splice(this.user.getCategoriesInc().indexOf(cat), 1);
+        }
         this.db.updateItem<User>(
             config.users_endpoint,
-            this.userData.getId(),
-            this.userData
+            this.user.getId(),
+            this.user
         );
     }
 }
