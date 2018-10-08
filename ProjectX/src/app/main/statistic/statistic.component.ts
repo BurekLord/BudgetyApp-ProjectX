b4ebc20b852ae43558e26ce1e59f637f984ed917
BurekLord <mile2389i@gmail.com>
2018-10-08 22:01:40 +0200

    statistics. working but not fully dinamic

--- ProjectX/src/app/main/statistic/statistic.component.ts
+++ ProjectX/src/app/main/statistic/statistic.component.ts
@@ -16,6 +16,11 @@ import { GoogleCharts } from 'google-charts';
     styleUrls: ['./statistic.component.scss']
 })
 export class StatisticComponent implements OnInit, OnChanges {
+
+    // TODO: sad kad radi treba ovo napraviti da bude dinamicko... picko
+
+    public static dataArr: any[] = [];
+
     user: User;
     expenses: Expense[];
     incomes: Income[];
@@ -33,39 +38,91 @@ export class StatisticComponent implements OnInit, OnChanges {
     }
 
     typeOfChart: string;
-    dataArr: any[];
 
     constructor() {}
 
     ngOnInit() {
-        this.dataArr = [];
+        StatisticComponent.dataArr = [];
     }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (this.expenses[0]) {
             console.log(this.expenses);
-            this.dataArr = [];
+            StatisticComponent.dataArr = [];
             this.expenses.forEach((expense) => {
-                this.dataArr.push(new StatisticDataModel(expense.getTimeStamp(), expense.getValue()));
+                StatisticComponent.dataArr.push(new StatisticDataModel(expense.getTimeStamp(), expense.getValue()));
             });
-            this.dataArr.sort((a, b) => (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0));
-            console.log(this.dataArr);
+            StatisticComponent.dataArr.sort((a, b) => (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0));
             GoogleCharts.load(this.drawTimeMoneyChart);
+            GoogleCharts.load('current', {packages: ['corechart']});
             // GoogleCharts.load(this.drawTimeMoneyChart);
         }
     }
 
     drawTimeMoneyChart() {
+
+        console.log('DATA ARR U DRAW', this);
+        console.log('pozvan draw');
         const transformedData = [['Time', 'Money']];
-        console.log(this.dataArr);
-        this.dataArr.forEach((el) => {
+        StatisticComponent.dataArr.forEach((el) => {
             transformedData.push([el.time, el.value]);
         });
         const chart = GoogleCharts.api.visualization.arrayToDataTable(transformedData);
         const lineChart = new GoogleCharts.api.visualization.LineChart(
             document.getElementById('chart1')
         );
-        lineChart.draw(chart);
+        const elementWidth = document.getElementById('statistics').offsetWidth; // ovo resenje nije respoonsiv
+        const options = {
+            lineWidth: 2.5,
+            pointSize: 5,
+            legend: 'left', // gde stoji legenda
+            title: 'Spending over time', // naslov
+            titleTextStyle: {
+                color: '#ffffff',
+                fontSize: 20,
+            },
+            width: elementWidth, // kad se ucita uzme duzinu statistik diva i da mu tu duzinu. ne resajzuje se
+            'backgroundColor': 'transparent',
+            // 'opacity' : '0.8', // ovo ne radi
+            // 'colors': '[red]', // ovo baguje zbog necega
+            'fontSize': 15,
+            chartArea: { // ovo kontrolise koliki je i poziciju area gde se nalazi chart. ako stavi 100% sve onda se ne vide slova sa strane
+                left: '5%',
+                top: '15%',
+                width: '90%',
+                height: '75%'
+            },
+            // curveType: 'function', // dal ce bude cik cak ili kurv
+            hAxis: { // stilizuje slova u horizontali
+                gridlines: { count: -1, color: '#606060' },
+                minorGridlines: { count: -1, color: '#606060' },
+                textStyle: {
+                color: '#ffffff',
+                fontSize: 12,
+                // bold: true,
+                // italic: true
+          }},
+          vAxis: { // stilizuje slova u vertikali
+                // ticks: [-1000, -5000, -10000, -20000, -50000, -100000], // bolje da ostavim automacki
+                direction: -1,
+                slantedTextAngle: 60,
+                format: 'currency',
+                gridlines: { count: -1, color: '#606060'}, // ovo treba da bude valjda koliko linija ima, al ne radi...
+                // baseline: 100,
+                baselineColor: 'green',
+                minorGridlines: {count: 100}, // nemam pojma sta radi ovo
+                textStyle: {
+                color: '#ffffff',
+                fontSize: 12,
+                // bold: true,
+                // italic: true
+          }},
+            explorer: {
+                // keepInBounds: true,
+            }, // ovo je da moze da se zum i pan
+            // theme: 'maximized',
+            };
+        lineChart.draw(chart, options);
     }
 }
 
