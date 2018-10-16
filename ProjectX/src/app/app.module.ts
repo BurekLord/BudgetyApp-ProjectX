bbd9202b699f05ae386a7324b3295ee25daf8fa0
BurekLord <mile2389i@gmail.com>
2018-10-16 09:39:48 +0200

    deleted total components

--- ProjectX/src/app/app.module.ts
+++ ProjectX/src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { TotalExpenseComponent } from './main/total-expense/total-expense.component';
-import { TotalIncomeComponent } from './main/total-income/total-income.component';
 import { CategoriesComponent } from './main/categories/categories.component';
 import { FormatMoneyPipe } from './../custom_pipes/format.money.pipe';
 import { StatisticComponent } from './main/statistic/statistic.component';
@@ -60,9 +58,7 @@ const appRoutes: Routes = [
         MainInputComponent,
         CategoriesComponent,
         FormatMoneyPipe,
-        PopupComponent,
-        TotalIncomeComponent,
-        TotalExpenseComponent
+        PopupComponent
     ],
     imports: [
         BrowserModule,
